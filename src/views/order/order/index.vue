2f3716c80bf76d63dfd1d04f74f5b40b53e3bba2
zhangjian <499980990@qq.com>
2024-01-14 12:05:05 +0800

    no message

---
+++ src/views/order/order/index.vue
@@ -0,0 +1,286 @@
+<template>
+  <div class="app-container">
+    <el-form :model="queryParams" ref="queryRef" :inline="true" v-show="showSearch" label-width="68px">
+      <el-form-item label="订单号" prop="orderId">
+        <el-input
+          v-model="queryParams.orderId"
+          placeholder="请输入订单号"
+          clearable
+          @keyup.enter="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="店铺名" prop="storeName">
+        <el-input
+          v-model="queryParams.storeName"
+          placeholder="请输入店铺名"
+          clearable
+          @keyup.enter="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="运单号" prop="postOrderId">
+        <el-input
+          v-model="queryParams.postOrderId"
+          placeholder="请输入运单号"
+          clearable
+          @keyup.enter="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="订单产品-属性" prop="goodsProperties">
+        <el-input
+          v-model="queryParams.goodsProperties"
+          placeholder="请输入订单产品-属性"
+          clearable
+          @keyup.enter="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="订单产品-数量" prop="goodsCount">
+        <el-input
+          v-model="queryParams.goodsCount"
+          placeholder="请输入订单产品-数量"
+          clearable
+          @keyup.enter="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" icon="Search" @click="handleQuery">搜索</el-button>
+        <el-button icon="Refresh" @click="resetQuery">重置</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-row :gutter="10" class="mb8">
+      <el-col :span="1.5">
+        <el-button
+          type="primary"
+          plain
+          icon="Plus"
+          @click="handleAdd"
+          v-hasPermi="['order:order:add']"
+        >新增</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="success"
+          plain
+          icon="Edit"
+          :disabled="single"
+          @click="handleUpdate"
+          v-hasPermi="['order:order:edit']"
+        >修改</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="danger"
+          plain
+          icon="Delete"
+          :disabled="multiple"
+          @click="handleDelete"
+          v-hasPermi="['order:order:remove']"
+        >删除</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="warning"
+          plain
+          icon="Download"
+          @click="handleExport"
+          v-hasPermi="['order:order:export']"
+        >导出</el-button>
+      </el-col>
+      <right-toolbar v-model:showSearch="showSearch" @queryTable="getList"></right-toolbar>
+    </el-row>
+
+    <el-table v-loading="loading" :data="orderList" @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55" align="center" />
+      <el-table-column label="id" align="center" prop="id" />
+      <el-table-column label="订单号" align="center" prop="orderId" />
+      <el-table-column label="店铺名" align="center" prop="storeName" />
+      <el-table-column label="运单号" align="center" prop="postOrderId" />
+      <el-table-column label="订单产品-属性" align="center" prop="goodsProperties" />
+      <el-table-column label="订单产品-数量" align="center" prop="goodsCount" />
+      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+        <template #default="scope">
+          <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)" v-hasPermi="['order:order:edit']">修改</el-button>
+          <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)" v-hasPermi="['order:order:remove']">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    
+    <pagination
+      v-show="total>0"
+      :total="total"
+      v-model:page="queryParams.pageNum"
+      v-model:limit="queryParams.pageSize"
+      @pagination="getList"
+    />
+
+    <!-- 添加或修改订单对话框 -->
+    <el-dialog :title="title" v-model="open" width="500px" append-to-body>
+      <el-form ref="orderRef" :model="form" :rules="rules" label-width="80px">
+        <el-form-item label="订单号" prop="orderId">
+          <el-input v-model="form.orderId" placeholder="请输入订单号" />
+        </el-form-item>
+        <el-form-item label="店铺名" prop="storeName">
+          <el-input v-model="form.storeName" placeholder="请输入店铺名" />
+        </el-form-item>
+        <el-form-item label="运单号" prop="postOrderId">
+          <el-input v-model="form.postOrderId" placeholder="请输入运单号" />
+        </el-form-item>
+        <el-form-item label="订单产品-属性" prop="goodsProperties">
+          <el-input v-model="form.goodsProperties" placeholder="请输入订单产品-属性" />
+        </el-form-item>
+        <el-form-item label="订单产品-数量" prop="goodsCount">
+          <el-input v-model="form.goodsCount" placeholder="请输入订单产品-数量" />
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <div class="dialog-footer">
+          <el-button type="primary" @click="submitForm">确 定</el-button>
+          <el-button @click="cancel">取 消</el-button>
+        </div>
+      </template>
+    </el-dialog>
+  </div>
+</template>
+
+<script setup name="Order">
+import { listOrder, getOrder, delOrder, addOrder, updateOrder } from "@/api/order/order";
+
+const { proxy } = getCurrentInstance();
+
+const orderList = ref([]);
+const open = ref(false);
+const loading = ref(true);
+const showSearch = ref(true);
+const ids = ref([]);
+const single = ref(true);
+const multiple = ref(true);
+const total = ref(0);
+const title = ref("");
+
+const data = reactive({
+  form: {},
+  queryParams: {
+    pageNum: 1,
+    pageSize: 10,
+    orderId: null,
+    storeName: null,
+    postOrderId: null,
+    goodsProperties: null,
+    goodsCount: null
+  },
+  rules: {
+    orderId: [
+      { required: true, message: "订单号不能为空", trigger: "blur" }
+    ],
+  }
+});
+
+const { queryParams, form, rules } = toRefs(data);
+
+/** 查询订单列表 */
+function getList() {
+  loading.value = true;
+  listOrder(queryParams.value).then(response => {
+    orderList.value = response.rows;
+    total.value = response.total;
+    loading.value = false;
+  });
+}
+
+// 取消按钮
+function cancel() {
+  open.value = false;
+  reset();
+}
+
+// 表单重置
+function reset() {
+  form.value = {
+    id: null,
+    orderId: null,
+    storeName: null,
+    postOrderId: null,
+    goodsProperties: null,
+    goodsCount: null
+  };
+  proxy.resetForm("orderRef");
+}
+
+/** 搜索按钮操作 */
+function handleQuery() {
+  queryParams.value.pageNum = 1;
+  getList();
+}
+
+/** 重置按钮操作 */
+function resetQuery() {
+  proxy.resetForm("queryRef");
+  handleQuery();
+}
+
+// 多选框选中数据
+function handleSelectionChange(selection) {
+  ids.value = selection.map(item => item.id);
+  single.value = selection.length != 1;
+  multiple.value = !selection.length;
+}
+
+/** 新增按钮操作 */
+function handleAdd() {
+  reset();
+  open.value = true;
+  title.value = "添加订单";
+}
+
+/** 修改按钮操作 */
+function handleUpdate(row) {
+  reset();
+  const _id = row.id || ids.value
+  getOrder(_id).then(response => {
+    form.value = response.data;
+    open.value = true;
+    title.value = "修改订单";
+  });
+}
+
+/** 提交按钮 */
+function submitForm() {
+  proxy.$refs["orderRef"].validate(valid => {
+    if (valid) {
+      if (form.value.id != null) {
+        updateOrder(form.value).then(response => {
+          proxy.$modal.msgSuccess("修改成功");
+          open.value = false;
+          getList();
+        });
+      } else {
+        addOrder(form.value).then(response => {
+          proxy.$modal.msgSuccess("新增成功");
+          open.value = false;
+          getList();
+        });
+      }
+    }
+  });
+}
+
+/** 删除按钮操作 */
+function handleDelete(row) {
+  const _ids = row.id || ids.value;
+  proxy.$modal.confirm('是否确认删除订单编号为"' + _ids + '"的数据项？').then(function() {
+    return delOrder(_ids);
+  }).then(() => {
+    getList();
+    proxy.$modal.msgSuccess("删除成功");
+  }).catch(() => {});
+}
+
+/** 导出按钮操作 */
+function handleExport() {
+  proxy.download('order/order/export', {
+    ...queryParams.value
+  }, `order_${new Date().getTime()}.xlsx`)
+}
+
+getList();
+</script>
