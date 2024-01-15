37eae75b50dad93edde6e66b32bb24e7f3f9cda5
zhangjian <499980990@qq.com>
2024-01-15 14:25:04 +0800

    no message

--- src/views/order/order/index.vue
+++ src/views/order/order/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="app-container">
-    <el-form :model="queryParams" ref="queryRef" :inline="true" v-show="showSearch" label-width="68px">
+    <el-form
+      :model="queryParams"
+      ref="queryRef"
+      :inline="true"
+      v-show="showSearch"
+      label-width="68px"
+    >
       <el-form-item label="订单号" prop="orderId">
         <el-input
           v-model="queryParams.orderId"
@@ -42,7 +48,9 @@
         />
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" icon="Search" @click="handleQuery">搜索</el-button>
+        <el-button type="primary" icon="Search" @click="handleQuery"
+          >搜索</el-button
+        >
         <el-button icon="Refresh" @click="resetQuery">重置</el-button>
       </el-form-item>
     </el-form>
@@ -55,7 +63,8 @@
           icon="Plus"
           @click="handleAdd"
           v-hasPermi="['order:order:add']"
-        >新增</el-button>
+          >新增</el-button
+        >
       </el-col>
       <el-col :span="1.5">
         <el-button
@@ -65,7 +74,8 @@
           :disabled="single"
           @click="handleUpdate"
           v-hasPermi="['order:order:edit']"
-        >修改</el-button>
+          >修改</el-button
+        >
       </el-col>
       <el-col :span="1.5">
         <el-button
@@ -75,7 +85,18 @@
           :disabled="multiple"
           @click="handleDelete"
           v-hasPermi="['order:order:remove']"
-        >删除</el-button>
+          >删除</el-button
+        >
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="info"
+          plain
+          icon="Upload"
+          @click="handleImport"
+          v-hasPermi="['system:user:import']"
+          >导入</el-button
+        >
       </el-col>
       <el-col :span="1.5">
         <el-button
@@ -84,29 +105,59 @@
           icon="Download"
           @click="handleExport"
           v-hasPermi="['order:order:export']"
-        >导出</el-button>
+          >导出</el-button
+        >
       </el-col>
-      <right-toolbar v-model:showSearch="showSearch" @queryTable="getList"></right-toolbar>
+      <right-toolbar
+        v-model:showSearch="showSearch"
+        @queryTable="getList"
+      ></right-toolbar>
     </el-row>
 
-    <el-table v-loading="loading" :data="orderList" @selection-change="handleSelectionChange">
+    <el-table
+      v-loading="loading"
+      :data="orderList"
+      @selection-change="handleSelectionChange"
+    >
       <el-table-column type="selection" width="55" align="center" />
       <el-table-column label="id" align="center" prop="id" />
       <el-table-column label="订单号" align="center" prop="orderId" />
       <el-table-column label="店铺名" align="center" prop="storeName" />
       <el-table-column label="运单号" align="center" prop="postOrderId" />
-      <el-table-column label="订单产品-属性" align="center" prop="goodsProperties" />
+      <el-table-column
+        label="订单产品-属性"
+        align="center"
+        prop="goodsProperties"
+      />
       <el-table-column label="订单产品-数量" align="center" prop="goodsCount" />
-      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+      <el-table-column
+        label="操作"
+        align="center"
+        class-name="small-padding fixed-width"
+      >
         <template #default="scope">
-          <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)" v-hasPermi="['order:order:edit']">修改</el-button>
-          <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)" v-hasPermi="['order:order:remove']">删除</el-button>
+          <el-button
+            link
+            type="primary"
+            icon="Edit"
+            @click="handleUpdate(scope.row)"
+            v-hasPermi="['order:order:edit']"
+            >修改</el-button
+          >
+          <el-button
+            link
+            type="primary"
+            icon="Delete"
+            @click="handleDelete(scope.row)"
+            v-hasPermi="['order:order:remove']"
+            >删除</el-button
+          >
         </template>
       </el-table-column>
     </el-table>
-    
+
     <pagination
-      v-show="total>0"
+      v-show="total > 0"
       :total="total"
       v-model:page="queryParams.pageNum"
       v-model:limit="queryParams.pageSize"
@@ -126,10 +177,16 @@
           <el-input v-model="form.postOrderId" placeholder="请输入运单号" />
         </el-form-item>
         <el-form-item label="订单产品-属性" prop="goodsProperties">
-          <el-input v-model="form.goodsProperties" placeholder="请输入订单产品-属性" />
+          <el-input
+            v-model="form.goodsProperties"
+            placeholder="请输入订单产品-属性"
+          />
         </el-form-item>
         <el-form-item label="订单产品-数量" prop="goodsCount">
-          <el-input v-model="form.goodsCount" placeholder="请输入订单产品-数量" />
+          <el-input
+            v-model="form.goodsCount"
+            placeholder="请输入订单产品-数量"
+          />
         </el-form-item>
       </el-form>
       <template #footer>
@@ -139,11 +196,64 @@
         </div>
       </template>
     </el-dialog>
+    <!-- 订单导入对话框 -->
+    <el-dialog
+      :title="upload.title"
+      v-model="upload.open"
+      width="400px"
+      append-to-body
+    >
+      <el-upload
+        ref="uploadRef"
+        :limit="1"
+        accept=".xlsx, .xls"
+        :headers="upload.headers"
+        :action="upload.url + '?updateSupport=' + upload.updateSupport"
+        :disabled="upload.isUploading"
+        :on-progress="handleFileUploadProgress"
+        :on-success="handleFileSuccess"
+        :auto-upload="false"
+        drag
+      >
+        <el-icon class="el-icon--upload"><upload-filled /></el-icon>
+        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+        <template #tip>
+          <div class="el-upload__tip text-center">
+            <div class="el-upload__tip">
+              <el-checkbox
+                v-model="upload.updateSupport"
+              />是否更新已经存在的用户数据
+            </div>
+            <span>仅允许导入xls、xlsx格式文件。</span>
+            <el-link
+              type="primary"
+              :underline="false"
+              style="font-size: 12px; vertical-align: baseline"
+              @click="importTemplate"
+              >下载模板</el-link
+            >
+          </div>
+        </template>
+      </el-upload>
+      <template #footer>
+        <div class="dialog-footer">
+          <el-button type="primary" @click="submitFileForm">确 定</el-button>
+          <el-button @click="upload.open = false">取 消</el-button>
+        </div>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
 <script setup name="Order">
-import { listOrder, getOrder, delOrder, addOrder, updateOrder } from "@/api/order/order";
+import { getToken } from "@/utils/auth";
+import {
+  listOrder,
+  getOrder,
+  delOrder,
+  addOrder,
+  updateOrder,
+} from "@/api/order/order";
 
 const { proxy } = getCurrentInstance();
 
@@ -166,13 +276,11 @@ const data = reactive({
     storeName: null,
     postOrderId: null,
     goodsProperties: null,
-    goodsCount: null
+    goodsCount: null,
   },
   rules: {
-    orderId: [
-      { required: true, message: "订单号不能为空", trigger: "blur" }
-    ],
-  }
+    orderId: [{ required: true, message: "订单号不能为空", trigger: "blur" }],
+  },
 });
 
 const { queryParams, form, rules } = toRefs(data);
@@ -180,7 +288,7 @@ const { queryParams, form, rules } = toRefs(data);
 /** 查询订单列表 */
 function getList() {
   loading.value = true;
-  listOrder(queryParams.value).then(response => {
+  listOrder(queryParams.value).then((response) => {
     orderList.value = response.rows;
     total.value = response.total;
     loading.value = false;
@@ -201,7 +309,7 @@ function reset() {
     storeName: null,
     postOrderId: null,
     goodsProperties: null,
-    goodsCount: null
+    goodsCount: null,
   };
   proxy.resetForm("orderRef");
 }
@@ -220,7 +328,7 @@ function resetQuery() {
 
 // 多选框选中数据
 function handleSelectionChange(selection) {
-  ids.value = selection.map(item => item.id);
+  ids.value = selection.map((item) => item.id);
   single.value = selection.length != 1;
   multiple.value = !selection.length;
 }
@@ -235,8 +343,8 @@ function handleAdd() {
 /** 修改按钮操作 */
 function handleUpdate(row) {
   reset();
-  const _id = row.id || ids.value
-  getOrder(_id).then(response => {
+  const _id = row.id || ids.value;
+  getOrder(_id).then((response) => {
     form.value = response.data;
     open.value = true;
     title.value = "修改订单";
@@ -245,16 +353,16 @@ function handleUpdate(row) {
 
 /** 提交按钮 */
 function submitForm() {
-  proxy.$refs["orderRef"].validate(valid => {
+  proxy.$refs["orderRef"].validate((valid) => {
     if (valid) {
       if (form.value.id != null) {
-        updateOrder(form.value).then(response => {
+        updateOrder(form.value).then((response) => {
           proxy.$modal.msgSuccess("修改成功");
           open.value = false;
           getList();
         });
       } else {
-        addOrder(form.value).then(response => {
+        addOrder(form.value).then((response) => {
           proxy.$modal.msgSuccess("新增成功");
           open.value = false;
           getList();
@@ -267,19 +375,78 @@ function submitForm() {
 /** 删除按钮操作 */
 function handleDelete(row) {
   const _ids = row.id || ids.value;
-  proxy.$modal.confirm('是否确认删除订单编号为"' + _ids + '"的数据项？').then(function() {
-    return delOrder(_ids);
-  }).then(() => {
-    getList();
-    proxy.$modal.msgSuccess("删除成功");
-  }).catch(() => {});
+  proxy.$modal
+    .confirm('是否确认删除订单编号为"' + _ids + '"的数据项？')
+    .then(function () {
+      return delOrder(_ids);
+    })
+    .then(() => {
+      getList();
+      proxy.$modal.msgSuccess("删除成功");
+    })
+    .catch(() => {});
 }
 
 /** 导出按钮操作 */
 function handleExport() {
-  proxy.download('order/order/export', {
-    ...queryParams.value
-  }, `order_${new Date().getTime()}.xlsx`)
+  proxy.download(
+    "order/order/export",
+    {
+      ...queryParams.value,
+    },
+    `order_${new Date().getTime()}.xlsx`
+  );
+}
+
+/*** 用户导入参数 */
+const upload = reactive({
+  // 是否显示弹出层（用户导入）
+  open: false,
+  // 弹出层标题（用户导入）
+  title: "",
+  // 是否禁用上传
+  isUploading: false,
+  // 是否更新已经存在的用户数据
+  updateSupport: 0,
+  // 设置上传的请求头部
+  headers: { Authorization: "Bearer " + getToken() },
+  // 上传的地址
+  url: import.meta.env.VITE_APP_BASE_API + "/order/order/importData",
+});
+/** 导入按钮操作 */
+function handleImport() {
+  upload.title = "订单导入";
+  upload.open = true;
+}
+/** 下载模板操作 */
+function importTemplate() {
+  proxy.download(
+    "order/order/importTemplate",
+    {},
+    `user_template_${new Date().getTime()}.xlsx`
+  );
+}
+/**文件上传中处理 */
+const handleFileUploadProgress = (event, file, fileList) => {
+  upload.isUploading = true;
+};
+/** 文件上传成功处理 */
+const handleFileSuccess = (response, file, fileList) => {
+  upload.open = false;
+  upload.isUploading = false;
+  proxy.$refs["uploadRef"].handleRemove(file);
+  proxy.$alert(
+    "<div style='overflow: auto;overflow-x: hidden;max-height: 70vh;padding: 10px 20px 0;'>" +
+      response.msg +
+      "</div>",
+    "导入结果",
+    { dangerouslyUseHTMLString: true }
+  );
+  getList();
+};
+/** 提交上传文件 */
+function submitFileForm() {
+  proxy.$refs["uploadRef"].submit();
 }
 
 getList();
